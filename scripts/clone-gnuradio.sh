ac983e1290b1b255e23fa8e44faa96fec8c25e0e
Alex Kreidler <alexkreidler2020@gmail.com>
2019-09-23 21:48:45 -0400

    reorganize docker

--- scripts/clone-gnuradio.sh
+++ scripts/clone-gnuradio.sh
@@ -3,4 +3,4 @@ echo "Cloning gnuradio repository ..."
 git clone -q --recurse-submodules --depth 1 https://github.com/gnuradio/gnuradio
 cd gnuradio
 git fetch -q --tags
-git checkout v3.8.0.0
+git -q checkout v3.8.0.0
